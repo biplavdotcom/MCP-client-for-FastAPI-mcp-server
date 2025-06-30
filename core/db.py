4aab88bb7a509cab202e6818f5516ea970f75263
biplavdotcom <lostking05@gmail.com>
2025-06-30 18:16:07 +0545

    added mongodb

---
+++ core/db.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+mongo_uri = os.environ.get("MONGO_DB_URL")
+try:
+    client = MongoClient(mongo_uri)
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
+    sys.exit(1)
+db = client["mcp-server"]
+
+collection = db["users"]
+
--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 from fastapi import FastAPI
 from fastapi_mcp import FastApiMCP
 from models import user
+from core.db import collection
 
 app = FastAPI()
 
-users = []
-
 @app.post("/signup", operation_id="signup")
 async def signup(username: str, password: str, email: str):
-    new_user = user.User(username=username, password=password, email=email)
-    users.append(new_user)
+    collection.insert_one({"username": username, "password": password, "email": email})
     return {"message": "User signed up successfully"}
 
 @app.post("/login", operation_id="login")
 async def login(username: str, password: str):
-    for user in users:
-        if user.username == username and user.password == password:
-            return {"message": "User logged in successfully"}
-        elif user.password != password:
-            return {"message": "Invalid password"}
+    user = collection.find_one({"username" : username})
+    if user and user["password"] == password:
+        return {"message": "User logged in successfully"}
     return {"message": "Invalid username or password"}
 
 @app.get("/clear_users", operation_id="clear_users")
 async def clear_users():
-    users.clear()
+    collection.delete_many({})
     return {"message": "Users cleared successfully"}
 
 @app.get("/users", operation_id="get_users")
 async def get_users():
-    # return [{"username": user.username, "password": user.password, "email": user.email} for user in users]
+    users = list(collection.find({}, {"_id": 0}))
     return users
 
 mcp = FastApiMCP(app)
